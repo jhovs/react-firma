354aa66c374a78085ce413dba7c90785f782b3ef
jhovan <jhovan.solano@gmail.com>
2022-08-30 17:53:12 -0600

    Firma React - sigCanvas.css | En Proceso

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+import SignaturePad from 'react-signature-canvas';
+import './sigCanvas.css'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Ejemplo de firma Finanova</h1>
+      
+      <Popup modal trigger={<button>Firma Aquí</button>}>
+        <SignaturePad
+          canvasProps={{
+            className: "signatureCanvas"
+          }}
+        /> 
+      </Popup>
     </div>
   );
 }
